efcee45bf3c7057ab949933614064b89151fa436
Pablo Cazorla <pablo.david.cazorla@gmail.com>
2014-02-05 18:04:15 -0300

    Changed all

--- js/jbook.js
+++ js/jbook.js
@@ -5,86 +5,122 @@
 	}
 	
 	jpage.prototype = {		
-		book : null,
+		
 		set : function(options){
 			this.cfg = $.extend({
-				sel : ''
+				selection : '.page',
+				duration : 1,
+				animations : []
 			}, options);
 			
-			this.$page = $(this.cfg.sel);
+			this.book = null;
+			this.ready = true;
 			
+			this.$window = $(window);
+			this.$page = $(this.cfg.selection);
 			
-			this.timecursor = 0;
-			
-			this.timeInit = 0;
-			this.timeEnd = 2014;
-			
-			
-			
-			this.dragging = false;
+			this.length = this.cfg.animations.length;			
+			this.parseAnimations();			
+			this.timeToPercent = 100/this.cfg.duration;			
+			this.setTime(0);
+			 
 			
 			
 			return this;
 		},
-		start : function(){
-			this.drag();
-			return this;
-		},
-		drag : function(){
-			var self = this,
-				dx = 0,
-				onLimitLeft = false;
-				onLimitRight = false;		
-			this.$page.mousedown(function(e){
-				self.book.off();			
-				dx = e.pageX + self.timecursor;
-				
-				onLimitLeft = false;
-				onLimitRight = false;
-				
-				if(self.timecursor == self.timeInit || self.timecursor == self.timeEnd){
-					onLimitLeft = true;
+		parseAnimations : function(){			
+			var parseArray = function(arr,defValue){
+				var frames01 = arr.split(','),frames02,frames03,frames04 = [], frames05 = [];					
+				for(var ie = 0; ie < frames01.length;++ie){
+					frames02 = frames01[ie].split(':');
+					frames03 = [parseInt(frames02[0]),parseInt(frames02[1])];
+					frames04.push(frames03);
 				}				
-				if(self.timecursor == self.timeEnd){
-					onLimitRight = true;
+				if(frames04[0][0]!=0){
+					var firstFrame = [0,defValue];				
+					frames04.splice(0,0,firstFrame);
 				}
-				
-				
-				self.dragging = true;				
-			});			
-			$('body')
-			.mousemove(function(e){
-				if(self.dragging){
-					var onLimitLeftMove = false,
-						onLimitRightMove = false;
-					
-					
-					
-					self.timecursor = dx - e.pageX;
-					
-					if(self.timecursor < self.timeInit){
-						self.timecursor = self.timeInit;
-						onLimitLeftMove = true;						
-					}
-					if(self.timecursor > self.timeEnd){
-						self.timecursor = self.timeEnd;
-						onLimitRightMove = true;						
-					}
-					
-					if((onLimitLeftMove && onLimitLeft) || (onLimitRightMove && onLimitRight)){
-						self.book.on();
-					}
-					
-					
-					
-					
-					console.log(self.timecursor);				
+				if(frames04[frames04.length-1][0]!=100){
+					var lastFrame = [100,frames04[frames04.length-1][1]];				
+					frames04.push(lastFrame);
+				}				
+				// Convert to linear ecuation
+				var l = frames04.length -1;
+				for(var iee = 0; iee < l;++iee){
+					var x1 = frames04[iee][0],
+						x2 = frames04[iee + 1][0],
+						y1 = frames04[iee][1],
+						y2 = frames04[iee + 1][1],
+						m = (y2 - y1)/(x2 - x1),				
+						b = y1 - (x1 * m),
+						ecuat = [x1,m,b];				
+					frames05.push(ecuat);
 				}
-			})
-			.mouseup(function(){			
-				self.dragging = false;
-			});
+				console.log(frames05);
+				return frames05;
+			}			
+			for(var i = 0;i < this.length;++i){								
+				this.cfg.animations[i] = $.extend({
+					selection : null,
+					moveX : 'none',
+					moveY : 'none',
+					opacity : 'none'
+				},this.cfg.animations[i]);
+				
+				this.cfg.animations[i].selection = this.$page.find(this.cfg.animations[i].selection);
+				if(this.cfg.animations[i].moveX != 'none'){										
+					this.cfg.animations[i].moveX = parseArray(this.cfg.animations[i].moveX, 100*parseInt(this.cfg.animations[i].selection.css('left'))/this.$window.width());
+					
+				}
+				if(this.cfg.animations[i].moveY != 'none'){										
+					this.cfg.animations[i].moveY = parseArray(this.cfg.animations[i].moveY,parseInt(100*this.cfg.animations[i].selection.css('top'))/this.$window.height());
+				}
+				if(this.cfg.animations[i].opacity != 'none'){										
+					this.cfg.animations[i].opacity = parseArray(this.cfg.animations[i].opacity,parseInt(this.cfg.animations[i].selection.css('opacity')));
+				}		
+			}
 			return this;
+		},
+		setTime : function(val){
+			this.timeCursor = this.timeToPercent * val;			
+			this.animate();
+			return this;
+		},
+		animate : function(){			 
+			for(var i = 0;i < this.length;++i){
+				if(this.cfg.animations[i].moveX != 'none'){
+					this.cfg.animations[i].selection.css({
+						'left':this.calcNewValue(this.cfg.animations[i].moveX)+'%'
+					});
+				}
+				if(this.cfg.animations[i].moveY != 'none'){
+					this.cfg.animations[i].selection.css({
+						'top':this.calcNewValue(this.cfg.animations[i].moveY)+'%'
+					});
+				}
+				if(this.cfg.animations[i].opacity != 'none'){
+					this.cfg.animations[i].selection.css({
+						'opacity':this.calcNewValue(this.cfg.animations[i].opacity)
+					});
+				}
+			}
+			return this;
+		},
+		calcNewValue : function(arr){
+			var l = arr.length, newValue;
+			for(var ie=0;ie<l;++ie){
+				try{
+					var topValue = arr[ie+1][0];
+				}catch(e){
+					var topValue = 100;
+				}						
+				if(this.timeCursor >= arr[ie][0] && this.timeCursor < topValue){							
+					// y = mx +b
+					newValue = arr[ie][1] * this.timeCursor + arr[ie][2];	
+					ie = 999999;
+				}
+			}
+			return newValue;
 		}
 	}
 	
@@ -92,157 +128,167 @@
 	if(!window.jpage) window.jpage = function(options){
 		return new jpage(options);
 	};
-	
-	
-	
+
 	/* BOOK */
 	var jbook = function(options){
 		this.set(options);
 	}
 	
 	jbook.prototype = {		
-		
-		set : function(options){
+		set : function(options){			
+			this.idSel = options || 'jb';			
 			
-			this.cfg = $.extend({
-				id : 'jb'
-			}, options);
-			
+			// Store Elements			
 			this.$window = $(window);			
-			this.$b = $('#'+this.cfg.id);			
-			this.$pages = this.$b.find('.page');
-						
+			this.$b = $('#'+this.idSel);						
+			this.$pages = this.$b.find('.page');						
 			this.$book = this.$b.find('.pageContainer').css('width',(this.$pages.length*100)+'%');			
 			
-			// Drag *************************
-			this.dragging = false;			
-			this.limit = 0;			
-		
-			// Animate ****************/	
-			this.x = 0;
-			this.xToGo = 0;
-			this.animateTimer = null;
-			this.velocity = .2;	
+			// Flags
+			this.dragging = false;
+			this.animating = false;
 			
-			// Change page ****************/
-			this.changingPage = false;
-			this.currentPage = 0;
-			this.limitChangePagePercent = .3;
-			
-			// Pages ****************/
+			// Variables
 			this.pages = [];
+			this.current = 0;
 			this.length = 0;
-			//
-			this.sizing().on();
+			this.timeCursor = 0;
+			this.difTime = 0;
+			this.timeline = [0];
+			this.mousepress = false;
+			this.x = 0;
+			this.dx = 0;
+			this.xToGo = 0;
+			this.limitChangePagePercent = .3;
+			this.animateTimer = null;
+			this.acceleration = .2;
+			
+			// Start
+			this.setTimeline().setSizes().setEvents();
 			
 			return this;
 		},
-		start : function(){
-			this.drag();
-			return this;
-		},		
-		add : function(page){			
-			page.book = this;
-			this.pages.push(page);
+		addPage : function(newPage){
+			newPage.book = this;
+			this.pages.push(newPage);
 			this.length++;
-			this.setSize();
-			return this;
-		},		
-		
-		on : function(){
-			this.active = true;
+			this.setTimeline().setSizes();
 			return this;
 		},
-		off : function(){
-			this.active = false;
+		setTimeline : function(){
+			this.timeline = [0];			
+			var countTime = 0;
+			for(var i = 0; i<this.length;i++){
+				countTime += this.pages[i].cfg.duration;
+				this.timeline.push(countTime);
+			}
 			return this;
 		},
-		drag : function(){			
-			var self = this,
-				dx = 0;			
-			this.$book.mousedown(function(e){
-				if(!self.changingPage){
-					dx = e.pageX - self.$book.offset().left;
-					self.dragging = true;
-				}
-			});			
-			$('body')
-			.mousemove(function(e){
-				if(self.dragging  && self.active){
-					self.x = e.pageX - dx;
-					if(self.x > self.limitL){
-						self.x = self.limitL;
-					}
-					if(self.x < self.limitR){
-						self.x = self.limitR;
-					}
-					self.$book.css('left', self.x +'px');					
-				}
-			})
-			.mouseup(function(){
-				if(self.dragging){
-					self.velocity = .2;
-					self.setPage().animate();
-				}
-				self.dragging = false;
-			});
+		setSizes : function(){
+			this.width = this.$window.width();
+			this.$pages.width(this.width);
+			this.limitChangePage = this.limitChangePagePercent * this.width;
+			this.x = -1 * this.current * this.width;
+			this.xToGo = this.x;
+			this.$book.css('left', this.x +'px');
+			this.maxX = 0;
+			this.minX = -1 * (this.length - 1) * this.width;
 			return this;
 		},
-		setPage : function(){
-			var prevPage = this.currentPage;
-			if(this.x < (this.xToGo - this.limitChangePage)) this.currentPage++;
-			if(this.x > (this.xToGo + this.limitChangePage)) this.currentPage--;			
-			if(prevPage != this.currentPage){
-				this.xToGo = -1 * this.currentPage * this.width;
-				this.velocity = .08;
-				this.changingPage = true;
+		setEvents : function(){
+			var self = this;
+			this.$book.mousedown(function(e){self.onMouseDown(e);});
+			$('body').mousemove(function(e){self.onMouseMove(e);}).mouseup(function(){self.onMouseUp();});
+			
+			this.$window.resize(function(){self.setSizes();});
+			return this;
+		},
+		onMouseDown : function(e){
+			if(!this.animating){
+				this.mousepress = true;
+				this.difTime = e.pageX + this.timeCursor;
 			}					
 			return this;
 		},
-		setSize : function(){
-			this.width = this.$window.width();
-			this.$pages.width(this.width);
-			this.limitL = this.limit* this.width;
-			this.limitR = -1*this.width*this.length+(1-this.limit)*this.width;					
-			this.limitChangePage = this.limitChangePagePercent * this.width;
-			this.x = -1 * this.currentPage * this.width;
-			this.xToGo = this.x;
+		onMouseMove : function(e){
+			if(this.mousepress) this.mouseMoving(e);
+			return this;
+		},
+		onMouseUp : function(){
+			this.mousepress = false;
+			if(this.dragging){
+				this.dx = null;
+				this.dragging = false;
+				this.setPage().animate();
+			}
+			return this;
+		},
+		mouseMoving : function(e){			
+			this.timeCursor = this.difTime - e.pageX;			
+			
+			if(this.timeCursor < this.timeline[this.current]){
+				this.timeCursor = this.timeline[this.current];
+				this.drag(e);
+			}else if(this.timeCursor > this.timeline[this.current+1]){
+				this.timeCursor = this.timeline[this.current+1];
+				this.drag(e);
+			}else{
+				this.dx = null;
+				this.dragging = false;
+				this.pages[this.current].setTime(this.timeCursor-this.timeline[this.current]);
+			}			
+			return this;
+		},
+		drag : function(e){
+			if(this.dx == null){
+				this.dx = e.pageX - this.$book.offset().left;
+				this.dragging = true;
+			}
+			this.x = e.pageX - this.dx;			
+			if(this.x > this.maxX) this.x = this.maxX;
+			if(this.x < this.minX) this.x = this.minX;
 			this.$book.css('left', this.x +'px');
 			return this;
 		},
-		sizing : function(){	
-			var self = this;
-			this.setSize().$window.resize(function(){self.setSize()});
+		setPage : function(){
+			var prevPage = this.current;
+			if(this.x < (this.xToGo - this.limitChangePage)) this.current++;
+			if(this.x > (this.xToGo + this.limitChangePage)) this.current--;			
+			if(prevPage != this.current){
+				this.xToGo = -1 * this.current * this.width;
+				this.changingPage = true;
+			}		
 			return this;
 		},
 		animate : function(){
 			var self = this,
-				dx = 0;			
+				difX = 0;
 			if(this.animateTimer == null){
 				// Start Animate				
 				this.animateTimer = setInterval(function(){
-					if(!self.dragging){	
-						// Animate
-						dx = self.velocity * (self.x - self.xToGo);
-						self.x = self.x - dx;						
-						if(Math.abs(dx) < .2){
-							self.x = self.xToGo;
-							dx = 0;
-						}						
-						self.$book.css('left', self.x +'px');						
-						if(dx == 0){
-							// Stop Animate
-							clearInterval(self.animateTimer);
-							self.animateTimer = null;
-							
-							if(self.changingPage){
-								self.changingPage = false;
-								self.off().pages[self.currentPage].start();
-							}					
-						}						
+					self.animating = true;
+					// Animate
+					difX =  (self.x - self.xToGo)*self.acceleration;
+					self.x = self.x - difX;						
+					if(Math.abs(difX) < .2){
+						self.x = self.xToGo;
+						difX = 0;
+					}						
+					self.$book.css('left', self.x +'px');						
+					if(difX == 0){
+						// Stop Animate
+						clearInterval(self.animateTimer);
+						self.animateTimer = null;
+						self.animating = false;
+						
+						if(self.changingPage){
+							self.changingPage = false;
+							// Accion de cambio de Pagina
+						}					
 					}
 				},20);
 			}
+			return this;
 		}
 	}
 	if(!window.jbook) window.jbook = function(options){
--- js/princess.js
+++ js/princess.js
@@ -1,17 +1,35 @@
 $('document').ready(function(){
 	
 	var princessBook = jbook(),
-		page1 = jpage({sel:'.page0'}),
-		page2 = jpage({sel:'.page1'}),
-		page3 = jpage({sel:'.page2'})
-		
-		
-		
-		;
+		page0 = jpage({
+			selection:'.page0',
+			duration : 10}),
+		page1 = jpage({
+			selection:'.page1',
+			duration : 1600,
+			animations : [
+				{
+					selection : '#sprite1',
+					moveX : '100:10',
+					opacity : '0:0,10:1',
+				}
+			]
+		}),
+		page2 = jpage({
+			selection:'.page2',
+			duration : 1000});
+		/*page1 = jpage({selection:'.page1',duration : 1600}),
+		page2 = jpage({selection:'.page2',duration : 800}),
+		page3 = jpage({selection:'.page3',duration : 2600}),
+		page4 = jpage({selection:'.page4',duration : 400}),
+		page5 = jpage({selection:'.page5',duration : 2100});*/
 	
 	princessBook
-	.add(page1)
-	.add(page2)
-	.add(page3)
-	.start();
+	.addPage(page0).addPage(page1).addPage(page2);
+	/*
+	.addPage(page2)
+	.addPage(page3)
+	.addPage(page4)
+	.addPage(page5);*/
+
 });
